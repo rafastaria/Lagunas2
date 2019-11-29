b0e69c895d00d4d404d6e2504253bf904e12a71a
rafastaria <guitronleafar@gmail.com>
2019-11-29 13:01:52 -0500

    removed unnecessary dependencies

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,18 +1,5 @@
 const { tesseract } = require("../libraries/index");
 
-/*function extract(image, config) {
-  new Promise(function(resolve, reject) {
-    tesseract
-      .recognize(image, config)
-      .then(text => {
-        resolve(text);
-      })
-      .catch(error => {
-        console.log(error.message);
-      });
-  });
-}*/
-
 var extract = function(image, config) {
   return new Promise(function(resolve, reject) {
     tesseract
